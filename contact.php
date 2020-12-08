35554b15e4e7e40d23e437270148cb36821d569f
Nadia Ait-Mouh <nadia.aitmouh@ensem.ac.ma>
2020-12-08 16:56:02 +0100

    modification

--- contact.php
+++ contact.php
@@ -37,6 +37,23 @@
 </head>
 
 <body>
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$phone = $_POST["userPhone"];
+	$content = $_POST["content"];
+
+	$toEmail = "nadia.aitmouh@ensem.ac.ma";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $phone, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact.php";
+?>
+
   <div class="js-animsition animsition" id="site-wrap" data-animsition-in-class="fade-in"
     data-animsition-out-class="fade-out">
 
@@ -100,7 +117,7 @@
       </div>
       <br>
       <form method="post" enctype="multipart/form-data"
-            onsubmit="return validateContactForm()">
+            onsubmit="return validateContactForm()" action="contact.php">
         <div class="row">
           <div class="col-md-6">
             <div class="form-group">
@@ -123,7 +140,7 @@
           </div>
           <div class="form-group">
               <input type="submit"  name="send"  class="btnSubmit btnContact" value="Send" />
-            </div>
+            
             <div id="statusMessage"> 
                         <?php
                         if (! empty($message)) {
@@ -313,22 +330,7 @@
         }
 </script>
 
-<?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
-	$subject = $_POST["userPhone"];
-	$content = $_POST["content"];
 
-	$toEmail = "nadia.aitmouh@ensem.ac.ma";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $phone, $content, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}
-}
-require_once "contact.php";
-?>
 </body>
 
 </html>
